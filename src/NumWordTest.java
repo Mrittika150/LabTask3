8e3a51393e8de6faee18a8e77f2a6ff4d486839c
Mriitika150 <mrittikajahan@iut-dhaka.edu>
2024-09-25 22:32:37 +0600

    Adding code

---
+++ src/NumWordTest.java
@@ -0,0 +1,48 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NumWordTest {
+
+    NumWordPrinter printer = new NumWordPrinter();
+
+
+    @Test
+    public void testDivisibleBy2() {
+        assertEquals("Even", printer.printWord(4, false));
+    }
+
+    @Test
+    public void testDivisibleBy11() {
+        assertEquals("EvenPrimeEleven", printer.printWord(22, false));
+    }
+
+
+    @Test
+    public void testDivisibleBy13() {
+        assertEquals("EVEN", printer.printWord(30, true));
+    }
+
+    @Test
+    public void testDivisibleBy2And11() {
+        assertEquals("EvenPrimeEleven", printer.printWord(22, false));
+    }
+
+    @Test
+    public void testDivisibleBy2And11And13() {
+        assertEquals("EvenPrimeLucky", printer.printWord(286, false));
+    }
+
+
+    @Test
+    public void testNotDivisibleByAny() {
+        assertEquals("OddOneOut", printer.printWord(7, false));
+    }
+
+
+    @Test
+    public void testUppercaseResult() {
+        assertEquals("EVEN", printer.printWord(4, true));
+    }
+
+}
